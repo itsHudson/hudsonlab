f2996d0ed104544a907e5f97f37f566c49eed808
NoNamee67 <hudson.officer23@gmail.com>
2026-03-08 19:41:56 +0800

    upgrade19

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,38 @@
 /* =========================================================
-   HUDSON PORTFOLIO - MASTER JS BUNDLE
+   HUDSON PORTFOLIO MASTER SCRIPT
    ========================================================= */
 
-/* =========================
-   CORE SCRIPTS
-   ========================= */
+document.addEventListener("DOMContentLoaded", () => {
 
-import "./core/component-loader.js";
-import "./core/navigation.js";
-import "./core/typing.js";
-import "./core/reveal.js";
-import "./core/scroll.js";
-import "./core/experience.js";
+    LoadScript("assets/js/core/component-loader.js", InitCore);
 
-/* =========================
-   FEATURES
-   ========================= */
+});
 
-import "./features/skill-globe.js";
-import "./features/project-modal.js";
+function InitCore(){
 
-/* =========================
-   PAGE LOGIC
-   ========================= */
+    LoadScript("assets/js/core/navigation.js");
+    LoadScript("assets/js/core/typing.js");
+    LoadScript("assets/js/core/reveal.js");
+    LoadScript("assets/js/core/scroll.js");
+    LoadScript("assets/js/core/experience.js");
 
-import "./pages/home.js";
+    LoadScript("assets/js/features/skill-globe.js");
+    LoadScript("assets/js/features/project-modal.js");
+
+    LoadScript("assets/js/pages/home.js");
+
+}
+
+function LoadScript(src, callback){
+
+    const script = document.createElement("script");
+
+    script.src = src;
+
+    script.onload = () => {
+        if(callback) callback();
+    };
+
+    document.body.appendChild(script);
+
+}
