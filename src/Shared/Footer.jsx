085eceff4e35f6c0dd62a77db7727d6a7b9923f5
NoNamee67 <hudson.officer23@gmail.com>
2026-03-08 23:12:40 +0800

    6

--- src/Shared/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="site-footer">
-      <p>© 2026 HUDSONLAB · Built by Hudson Chia</p>
-    </footer>
-  );
-}
--- src/Shared/PageTransition.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function PageTransition({ children }) {
-  return <main className="page-transition">{children}</main>;
-}
